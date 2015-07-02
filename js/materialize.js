56becfe0d129b268d873b9898248ba70406acbf6
Angel <angel_gnz@outlook.com>
2015-07-02 00:48:54 -0500

    Modificacion de comportamiento boton fixed que despliega menu

--- js/materialize.js
+++ js/materialize.js
@@ -3167,31 +3167,63 @@ $(document).ready(function(){
     // jQuery reverse
     $.fn.reverse = [].reverse;
 
-    $(document).on('mouseenter.fixedActionBtn', '.fixed-action-btn', function(e) {
-      var $this = $(this);
+//    $(document).on('mouseenter.fixedActionBtn', '.fixed-action-btn', function(e) {
+//      var $this = $(this);
+//
+//      $this.find('ul .btn-floating').velocity(
+//        { scaleY: ".4", scaleX: ".4", translateY: "40px"},
+//        { duration: 0 });
+//
+//      var time = 0;
+//      $this.find('ul .btn-floating').reverse().each(function () {
+//        $(this).velocity(
+//          { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+//          { duration: 80, delay: time });
+//        time += 40;
+//      });
+//
+//    });
+//
+//    $(document).on('mouseleave.fixedActionBtn', '.fixed-action-btn', function(e) {
+//      var $this = $(this);
+//
+//      var time = 0;
+//      $this.find('ul .btn-floating').velocity("stop", true);
+//      $this.find('ul .btn-floating').velocity(
+//        { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+//        { duration: 80 });
+//    });
+      //**** Arriba se comento para dar paso al codigo con el que se da funcion de toggle a los fixed action button***///
+      $(document).on('click.fixedActionBtn', '.fixed-action-btn', function(e) {
+        var state = $(this).data('state');
+        var $this = $(this);
+        switch(state){
+            case 1 :
+            case undefined : 
+                
+                $this.find('ul .btn-floating').velocity(
+                    { scaleY: ".4", scaleX: ".4", translateY: "40px"},
+                    { duration: 0 });
 
-      $this.find('ul .btn-floating').velocity(
-        { scaleY: ".4", scaleX: ".4", translateY: "40px"},
-        { duration: 0 });
+              var time = 0;
+              $this.find('ul .btn-floating').reverse().each(function () {
+                $(this).velocity(
+                  { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+                  { duration: 80, delay: time });
+                time += 40;
+              }); 
+              $(this).data('state', 2); break;
+                
+            case 2 : 
+              var time = 0;
+              $this.find('ul .btn-floating').velocity("stop", true);
+              $this.find('ul .btn-floating').velocity(
+                { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+                { duration: 80 });
+              $(this).data('state', 1);break;
+        }
+      
 
-      var time = 0;
-      $this.find('ul .btn-floating').reverse().each(function () {
-        $(this).velocity(
-          { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
-          { duration: 80, delay: time });
-        time += 40;
-      });
-
-    });
-
-    $(document).on('click.fixedActionBtn', '.fixed-action-btn', function(e) {
-      var $this = $(this);
-
-      var time = 0;
-      $this.find('ul .btn-floating').velocity("stop", true);
-      $this.find('ul .btn-floating').velocity(
-        { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
-        { duration: 80 });
     });
 
   });
